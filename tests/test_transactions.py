706d4645c52045960baa764efff7504c794d42d8
Šarūnas Navickas <zaibacu@gmail.com>
2023-06-30 19:24:56 +0300

    Cleaning accidental tabs

--- tests/test_transactions.py
+++ tests/test_transactions.py
@@ -8,27 +8,27 @@ from simple_accounting.models import Ledger, LedgerEntry, Transaction
 
 @pytest.mark.django_db
 def test_balanced_transaction():
-	admin = User.objects.get(email="admin@localhost")
-	t = Transaction.objects.create(transaction_type=Transaction.TRANSACTION_AUTOMATED, posted_timestamp=datetime.now(), owner=admin)
+    admin = User.objects.get(email="admin@localhost")
+    t = Transaction.objects.create(transaction_type=Transaction.TRANSACTION_AUTOMATED, posted_timestamp=datetime.now(), owner=admin)
 
-	cash_ledger = Ledger.objects.get(name="Cash")
-	investments_ledger = Ledger.objects.get(name="Investments")
+    cash_ledger = Ledger.objects.get(name="Cash")
+    investments_ledger = Ledger.objects.get(name="Investments")
 
-	LedgerEntry.objects.create(transaction=t, ledger=cash_ledger, debit=5000)
-	LedgerEntry.objects.create(transaction=t, ledger=investments_ledger, credit=5000)
+    LedgerEntry.objects.create(transaction=t, ledger=cash_ledger, debit=5000)
+    LedgerEntry.objects.create(transaction=t, ledger=investments_ledger, credit=5000)
 
-	assert t.balanced
+    assert t.balanced
 
 
 @pytest.mark.django_db
 def test_unbalanced_transaction():
-	admin = User.objects.get(email="admin@localhost")
-	t = Transaction.objects.create(transaction_type=Transaction.TRANSACTION_AUTOMATED, posted_timestamp=datetime.now(), owner=admin)
+    admin = User.objects.get(email="admin@localhost")
+    t = Transaction.objects.create(transaction_type=Transaction.TRANSACTION_AUTOMATED, posted_timestamp=datetime.now(), owner=admin)
 
-	cash_ledger = Ledger.objects.get(name="Cash")
-	investments_ledger = Ledger.objects.get(name="Investments")
+    cash_ledger = Ledger.objects.get(name="Cash")
+    investments_ledger = Ledger.objects.get(name="Investments")
 
-	LedgerEntry.objects.create(transaction=t, ledger=cash_ledger, debit=5000)
-	LedgerEntry.objects.create(transaction=t, ledger=investments_ledger, credit=5001)
+    LedgerEntry.objects.create(transaction=t, ledger=cash_ledger, debit=5000)
+    LedgerEntry.objects.create(transaction=t, ledger=investments_ledger, credit=5001)
 
-	assert not t.balanced
+    assert not t.balanced
